bb392f7f2bf14192b5953c19bae797f1ade1026b
Johnllli <161318985+Johnllli@users.noreply.github.com>
2025-04-23 22:04:12 +0200

    update ajax
    complete ajax?

--- jsses/ajax.js
+++ jsses/ajax.js
@@ -1,10 +1,8 @@
-const code = "EXVVC8dxt465"; 
+const code = "EXVVC8dxt465";
 const url = "http://gamf.nhely.hu/ajax2/";
 
-// READ
 async function read() {
   document.getElementById("code").innerHTML = "code=" + code;
-
   let response = await fetch(url, {
     method: 'post',
     cache: 'no-cache',
@@ -13,58 +11,41 @@ async function read() {
     },
     body: "code=" + code + "&op=read"
   });
-
   let data = await response.text();
   data = JSON.parse(data);
   let list = data.list;
-
-  let str = "<h1></h1>";
+  let str = "<h1>Read</h1>";
   str += "<p>Number of records: " + data.rowCount + "</p>";
   str += "<p>Last max " + data.maxNum + " records:</p>";
-  str += "<table><tr><th>id</th><th>name</th><th>age</th><th>favfruit</th><th>code</th></tr>";
-
+  str += "<table class='table'><tr><th>id</th><th>name</th><th>age</th><th>favfruit</th><th>code</th></tr>";
   for (let i = 0; i < list.length; i++) {
-    str += "<tr><td>" + list[i].id + "</td><td>" + list[i].name + "</td><td>" + list[i].age + "</td><td>" + list[i].favfruit + "</td><td>" + list[i].code + "</td></tr>";
+    str += "<tr><td>" + list[i].id + "</td><td>" + list[i].name + "</td><td>" + list[i].height + "</td><td>" + list[i].weight + "</td><td>" + list[i].code + "</td></tr>";
   }
-
   str += "</table>";
-
-  // Optional: summary for age
-  let ages = list.map(item => Number(item.age));
-  let sum = ages.reduce((a, b) => a + b, 0);
-  let avg = (sum / ages.length).toFixed(2);
-  let max = Math.max(...ages);
-
-  str += "<p><strong>Sum of ages:</strong> " + sum + "</p>";
-  str += "<p><strong>Average age:</strong> " + avg + "</p>";
-  str += "<p><strong>Max age:</strong> " + max + "</p>";
-
   document.getElementById("readDiv").innerHTML = str;
 }
 
-// CREATE
 async function create() {
-  let name = document.getElementById("name1").value;
+  let nameStr = document.getElementById("name1").value;
   let age = document.getElementById("age1").value;
   let favfruit = document.getElementById("favfruit1").value;
-
-  if (name.length > 0 && name.length <= 30 &&
-      age.length > 0 && age.length <= 30 &&
-      favfruit.length > 0 && favfruit.length <= 30) {
-
+  if (
+    nameStr.length > 0 && nameStr.length <= 30 &&
+    age.length > 0 && age.length <= 30 &&
+    favfruit.length > 0 && favfruit.length <= 30 &&
+    code.length <= 30
+  ) {
     let response = await fetch(url, {
       method: 'post',
       cache: 'no-cache',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
-      body: "code=" + code + "&op=create&name=" + name + "&age=" + age + "&favfruit=" + favfruit
+      body: "code=" + code + "&op=create&name=" + nameStr + "&height=" + age + "&weight=" + favfruit
     });
-
     let data = await response.text();
-    let str = (data > 0) ? "Create successful!" : "Create NOT successful!";
+    let str = (parseInt(data) > 0) ? "Create successful!" : "Create NOT successful!";
     document.getElementById("createResult").innerHTML = str;
-
     document.getElementById("name1").value = "";
     document.getElementById("age1").value = "";
     document.getElementById("favfruit1").value = "";
@@ -74,7 +55,6 @@ async function create() {
   }
 }
 
-// GET DATA FOR ID
 async function getDataForId() {
   let response = await fetch(url, {
     method: 'post',
@@ -84,45 +64,41 @@ async function getDataForId() {
     },
     body: "code=" + code + "&op=read"
   });
-
   let data = await response.text();
   data = JSON.parse(data);
   let list = data.list;
-
   for (let i = 0; i < list.length; i++) {
     if (list[i].id == document.getElementById("idUpd").value) {
       document.getElementById("name2").value = list[i].name;
-      document.getElementById("age2").value = list[i].age;
-      document.getElementById("favfruit2").value = list[i].favfruit;
+      document.getElementById("age2").value = list[i].height;
+      document.getElementById("favfruit2").value = list[i].weight;
     }
   }
 }
 
-// UPDATE
 async function update() {
   let id = document.getElementById("idUpd").value;
-  let name = document.getElementById("name2").value;
+  let nameStr = document.getElementById("name2").value;
   let age = document.getElementById("age2").value;
   let favfruit = document.getElementById("favfruit2").value;
-
-  if (id.length > 0 && id.length <= 30 &&
-      name.length > 0 && name.length <= 30 &&
-      age.length > 0 && age.length <= 30 &&
-      favfruit.length > 0 && favfruit.length <= 30) {
-
+  if (
+    id.length > 0 && id.length <= 30 &&
+    nameStr.length > 0 && nameStr.length <= 30 &&
+    age.length > 0 && age.length <= 30 &&
+    favfruit.length > 0 && favfruit.length <= 30 &&
+    code.length <= 30
+  ) {
     let response = await fetch(url, {
       method: 'post',
       cache: 'no-cache',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
-      body: "code=" + code + "&op=update&id=" + id + "&name=" + name + "&age=" + age + "&favfruit=" + favfruit
+      body: "code=" + code + "&op=update&id=" + id + "&name=" + nameStr + "&height=" + age + "&weight=" + favfruit
     });
-
     let data = await response.text();
-    let str = (data > 0) ? "Update successful!" : "Update NOT successful!";
+    let str = (parseInt(data) > 0) ? "Update successful!" : "Update NOT successful!";
     document.getElementById("updateResult").innerHTML = str;
-
     document.getElementById("idUpd").value = "";
     document.getElementById("name2").value = "";
     document.getElementById("age2").value = "";
@@ -133,10 +109,8 @@ async function update() {
   }
 }
 
-// DELETE
 async function deleteF() {
   let id = document.getElementById("idDel").value;
-
   if (id.length > 0 && id.length <= 30) {
     let response = await fetch(url, {
       method: 'post',
@@ -146,11 +120,9 @@ async function deleteF() {
       },
       body: "code=" + code + "&op=delete&id=" + id
     });
-
     let data = await response.text();
-    let str = (data > 0) ? "Delete successful!" : "Delete NOT successful!";
+    let str = (parseInt(data) > 0) ? "Delete successful!" : "Delete NOT successful!";
     document.getElementById("deleteResult").innerHTML = str;
-
     document.getElementById("idDel").value = "";
     read();
   } else {
@@ -158,7 +130,6 @@ async function deleteF() {
   }
 }
 
-// Auto-load on page load
-window.onload = function () {
-  read();
+window.onload = function () {
+  read();
 };
